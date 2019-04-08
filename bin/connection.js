58e6b152b44ac25baffc64752918a82b9d3395c1
RodrigoLS <rolima13@gmail.com>
2019-04-08 09:59:52 -0300

    connect comment

--- bin/connection.js
+++ bin/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/mypetpass`, {useNewUrlParser: true})
+// mongoose.connect(`mongodb://localhost:27017/mypetpass`, {useNewUrlParser: true})
 
 module.exports = mongoose
