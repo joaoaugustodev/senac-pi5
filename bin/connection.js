26b886aca13c6578e1180e046d8fdf6aac162b2d
João Ribeiro <jlribeiroaugusto@gmail.com>
2019-04-15 21:44:17 -0300

    ajustes de conexao com o mongoose

--- bin/connection.js
+++ bin/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/mypetpass`, {useNewUrlParser: true})
+mongoose.connect(`mongodb+srv://mypetpass:segredodopetpass@cluster0-hygow.mongodb.net/test`, {useNewUrlParser: true})
 
 module.exports = mongoose
