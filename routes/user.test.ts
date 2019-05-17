8e62884c55aa49bc9a4f7ae1be659bd1cec40077
marcelob133 <marceloborellihegedus@gmail.com>
2019-05-17 19:17:31 -0300

    novos testes

--- routes/user.test.ts
+++ routes/user.test.ts
@@ -1,11 +1,13 @@
 import 'jest'
 import * as request from 'supertest'
 
+//const endPoint = 'localhost:3000'
+const endPoint = 'http://f721bc95.ngrok.io'
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTWFyY2VsbyIsImlhdCI6MTU1NzQ0MDE1Nn0.rILKnhwrVXCgW4vdorpzpBHlLaiJWjBLyDRhSP8rk2s"
 
-test('get user/comments', ()=>{
-    return request('https://2bad5f79.ngrok.io')
-    .get('/user/animals/5ca799dadf0348617014c496')
+test('Comentarios do Owner - retorno  200', ()=>{
+    return request(endPoint)
+    .get('/user/animals/5ca8c905725d7b51b271c31e')
     .set('Authorization', 'Bearer ' + token)
     .then(response=>{
         expect(response.status).toBe(200)
