92663c5112853232636c72da69066970414fc8a9
RodrigoLS <rolima13@gmail.com>
2019-06-10 20:54:53 -0300

    cloud db

--- bin/connection.js
+++ bin/connection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://localhost:27017/mypetpass`, {useNewUrlParser: true})
+// mongoose.connect(`mongodb://localhost:27017/mypetpass`, {useNewUrlParser: true})
 //mongoose.connect(`mongodb+srv://mypetpass:segredodopetpass@cluster0-hygow.mongodb.net/test`, {useNewUrlParser: true})
 
+mongoose.connect(`mongodb+srv://adrianotameouvindo:segredodopetpass@cluster-mypetpass-khguj.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true})
+
 module.exports = mongoose
+
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9iYmVyIEJhbmhvIDAxIiwiaWF0IjoxNTYwMjA5NDg4fQ.s1I3kJrZIFD9baVU-j_RayrwxMwkHdTFpyiDz3nRzAE
